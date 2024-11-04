aa35ab0aadd04c1635ca694c559c74f0179253b7
Benjamin Velagic <benjamin.velagic@gmail.com>
2024-11-04 18:00:30 +0100

    input in zehner und einer unterteilt, boundaries (0-100) eingeführt

--- romanEncoder/src/main/java/org/example/RomanNumerals.java
+++ romanEncoder/src/main/java/org/example/RomanNumerals.java
@@ -3,34 +3,38 @@ package org.example;
 public class RomanNumerals {
     public static String intToRoman(int number)
     {
-        StringBuilder output = new StringBuilder();
-        if(10 <= number && number <= 13)
+        if(number == 0)
         {
-            output.append("X");
-            for (int i = 10; i <= number; i++)
-            {
-                output.append("I");
-            }
+            return "";
         }
-        if(number == 9)
+        if(number > 99)
+        {
+            return "Zu groß";
+        }
+        StringBuilder output = new StringBuilder();
+
+        int einer = number % 10;
+        int zehner = (number - einer)/10;
+
+        if(einer == 9)
         {
             output.append("IX");
         }
-        if(5 <= number && number <= 8)
+        if(5 <= einer && einer <= 8)
         {
             output.append("V");
-            for (int i = 6; i <= number; i++)
+            for (int i = 6; i <= einer; i++)
             {
                 output.append("I");
             }
         }
-        if(number == 4)
+        if(einer == 4)
         {
             output.append("IV");
         }
-        if(number < 4)
+        if(einer < 4)
         {
-            for (int i = 1; i <= number; i++)
+            for (int i = 1; i <= einer; i++)
             {
                 output.append("I");
             }
